f53a2daa1bd19ff8c7fecfd84b03e2ef008a891b
Rahul <rahu8299@gmail.com>
2025-07-09 11:04:54 +0530

    setting up frontend and routes

--- internal/router/router.go
+++ internal/router/router.go
@@ -2,14 +2,24 @@ package router
 
 import (
 	"go_learn/internal/handler"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
 	engine := gin.Default()
+	engine.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
-	engine.GET("/users", handler.GetUsers)
-	engine.POST("/users", handler.CreateUser)
+	engine.GET("/api/users", handler.GetUsers)
+	engine.POST("/api/users", handler.CreateUser)
 	return engine
 }
